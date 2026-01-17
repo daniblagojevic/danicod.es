99dfa468744bc6bb39aa92181c8bd35e1d012792
Danijel <danijelblagojevic431@gmail.com>
2026-01-17 01:12:19 +0100

    Test: change button color

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -130,7 +130,7 @@ export const FormBuilder: React.FC<FormBuilderProps> = ({ form }) => {
                                 </div>
                             ))}
                             <div className="col-span-10">
-                                <Button type="submit" className="bg-red-500">
+                                <Button type="submit" className="bg-green-500">
                                     {form.submitButtonLabel}
                                 </Button>
                             </div>
